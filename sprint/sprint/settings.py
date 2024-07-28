5ed2ee615e9fef7b86f7b3a272c261d07010f2e4
stds58 <stds58@gmail.com>
2024-07-29 01:32:38 +0300

    docker

--- sprint/sprint/settings.py
+++ sprint/sprint/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-2z6%z=g_1b-slkjfvgc-zq&=c5(ip854d7-%)0f29vj18_54p8'
+SECRET_KEY = 'django-insecure-2z6%z=g_1b-slkjfvgc-zq&=c5(ip854d7-%)0f29vj18_54p8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = config("DEBUG", default=False, cast=bool)
-SECRET_KEY = config("SECRET_KEY", default="")
+DEBUG = True
+#DEBUG = config("DEBUG", default=False, cast=bool)
+#SECRET_KEY = config("SECRET_KEY", default="")
 
 ALLOWED_HOSTS = [
     '0.0.0.0',
