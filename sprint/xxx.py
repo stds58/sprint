bb655b0f827e5217a235af5911b1c20acb30536e
stds58 <stds58@gmail.com>
2024-07-28 19:32:31 +0300

    docker

---
+++ sprint/xxx.py
@@ -0,0 +1,139 @@
+
+сделать
+
+Проект опубликован на хостинге pythonanywhere.com
+(поменять путь)
+API Pereval: https://olegafonick.pythonanywhere.com/Pereval/
+Документация swagger: https://olegafonick.pythonanywhere.com/swagger/
+Документация redoc: https://olegafonick.pythonanywhere.com/redoc/
+
+Отчет о покрытии тестами:
+
+
+
+
+{
+    "beauty_title": "n",
+    "title": "n",
+    "other_title": "n",
+    "connect": "n",
+    "images": [{"title":"title1", "img":"https://pereval.com/img1.jpg"}],
+    "user": {
+        "name": "n",
+        "fam": "n",
+        "otc": "n",
+        "phone": "n",
+        "email": "ndd@ff.tr"
+    },
+    "coord": {
+        "latitude": 10.00,
+        "longitude": 10.00,
+        "height": 10
+    },
+    "level": {
+        "winter": "1A",
+        "summer": "1A",
+        "autumn": "1A",
+        "spring": "1A"
+    }
+}
+
+{
+    "beauty_title": "0",
+    "title": "gfhn",
+    "other_title": "n",
+    "connect": "0",
+    "images": [
+        {
+            "title": "title30",
+            "img": "https://pereval.com/img30.jpg"
+        },
+        {
+            "title": "title40",
+            "img": "https://pereval.com/img40.jpg"
+        }
+    ],
+    "user": {
+        "name": "n",
+        "fam": "n",
+        "otc": "n",
+        "phone": "n",
+        "email": "nrrd@ff.tr"
+    },
+    "coord": {
+        "latitude": 13.00,
+        "longitude": 13.00,
+        "height": 13
+    },
+    "level": {
+        "winter": "1A",
+        "summer": "1A",
+        "autumn": "1A",
+        "spring": "1A"
+    }
+}
+
+
+
+
+
+{
+    "beauty_title": "d",
+    "title": "d",
+    "other_title": "d",
+    "connect": "d",
+    "images": [{
+            "title": "title30",
+            "img": "www.pereval.com/img4.jpg"
+        }],
+    "user": {
+        "name": "n",
+        "fam": "n",
+        "otc": "n",
+        "phone": "de",
+        "email": "yyy@ff.tr"
+    },
+    "coord": {
+        "latitude": 13.00,
+        "longitude": 13.00,
+        "height": 13
+    },
+    "level": {
+        "winter": "1A",
+        "summer": "1A",
+        "autumn": "1A",
+        "spring": "1A"
+    }
+}
+
+
+
+
+
+{
+    "beauty_title": "n",
+    "title": "n",
+    "other_title": "n",
+    "connect": "n",
+    "user": {
+        "name": "n",
+        "fam": "n",
+        "otc": "n",
+        "phone": "n",
+        "email": "nvfd@ff.jh"
+    },
+    "coord": {
+        "latitude": 10,
+        "longitude": 10,
+        "height": 10
+    },
+    "level": {
+        "winter": "1A",
+        "summer": "1A",
+        "autumn": "1A",
+        "spring": "1A"
+    }
+}
+
+
+{"latitude": "2.00","longitude": "2.00","height": 2}
